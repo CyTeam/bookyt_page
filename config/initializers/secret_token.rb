c3b6b57fb9f2c4a60441b07b4a9039e16747eb11
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-17 22:13:16 +0100

    Fresh rails 3.2.1.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookytPage::Application.config.secret_token = 'e7d6a8b1fb15043f3b96a2c74127280c43b835d55d24ba2a052954ff09abcb4ee823d7b0e7310147f4c729285e9baf120e22a0ba3b00881814e6ffc12dea3ac3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BookytPage::Application.config.session_store :cookie_store, :key => '_bookyt_page_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BookytPage::Application.config.session_store :active_record_store
