514dd51c8a594522148c581160a7c5878c1bb38f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-19 00:26:17 +0100

    Capify.

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+# Application
+set :application, 'bookyt_page'
+set :repository,  'git@github.com:huerlisi/bookyt_page.git'
+
+require 'capones_recipes/cookbook/rails'
+require 'capones_recipes/tasks/database/setup'
+
+load 'deploy/assets'
+
+# Staging
+set :default_stage, 'production'
+
+# Deployment
+set :server, :passenger
+set :user, "deployer"                               # The server's user for deploys
+
+# Sync directories
+#set :sync_directories, ['uploads']
+#set :sync_backups, 3
+
+# Configuration
+set :scm, :git
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+set :copy_exclude, [".git", "spec"]
