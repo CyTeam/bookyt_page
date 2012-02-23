0419f02b8207ce8f5a5022670187e1bd8fcd05db
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-23 17:13:41 +0100

    Use translation for app title.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Set the user locale
+  before_filter :set_locale
+  def set_locale
+    locale = params[:locale] || cookies[:locale]
+    I18n.locale = locale.to_s
+    cookies[:locale] = locale unless (cookies[:locale] && cookies[:locale] == locale)
+  end
 end
