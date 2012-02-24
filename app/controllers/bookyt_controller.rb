eb80be6b4f5aded074cd4dde8af50019b0da8cd6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-24 23:29:21 +0100

    Fixup kmu and treuhand links.

--- app/controllers/bookyt_controller.rb
+++ app/controllers/bookyt_controller.rb
@@ -1,9 +1,9 @@
 class BookytController < ApplicationController
   def kmu
-    redirect_to 'tour'
+    redirect_to '/tour'
   end
 
   def treuhand
-    redirect_to 'features'
+    redirect_to '/features'
   end
 end
