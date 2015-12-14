e6b9e4d81e700f274778d55bd699f55ae0df9f41
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-12-14 21:59:24 +0100

    Fix default locale setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   # Set the user locale
   before_filter :set_locale
   def set_locale
-    locale = params[:locale] || cookies[:locale]
+    locale = params[:locale].presence || cookies[:locale].presence || I18n.default_locale
     I18n.locale = locale.to_s
-    cookies[:locale] = locale unless (cookies[:locale] && cookies[:locale] == locale)
+    cookies[:locale] = locale unless (cookies[:locale] && cookies[:locale] == locale.to_s)
   end
 end
